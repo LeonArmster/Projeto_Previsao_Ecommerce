75b0de165163b4c5ff64a67fc74cfb75e6059d98
LeonArmster <leo.armster1994@gmail.com>
2026-03-06 00:43:37 -0300

    Inicio de projeto

---
+++ Code/config.py
@@ -0,0 +1,11 @@
+# Biblioteca
+from pathlib import Path
+
+# Caminho da pasta
+caminho = Path(__file__).resolve().parent.parent
+
+# Pasta de dados
+data = caminho/'Data'
+
+# Pasta de imagens
+imagem = caminho/'Image'
